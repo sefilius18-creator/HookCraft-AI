cebbf088518b6c00c18275c3a0a19a4433a4f61c
sefilius18-creator <sefiliusjunianto1838@gmail.com>
2026-06-01 16:09:10 +0700

    Create prompts.py

---
+++ services/prompts.py
@@ -0,0 +1,26 @@
+def hook_prompt(topic, hook_type):
+
+    return f"""
+Anda adalah copywriter TikTok profesional.
+
+Buatkan 10 hook viral.
+
+TOPIK:
+{topic}
+
+TIPE:
+{hook_type}
+
+ATURAN:
+
+- Bahasa Indonesia
+- Maksimal 15 kata
+- Menarik perhatian
+- Format nomor
+
+Output:
+
+1.
+2.
+3.
+"""
